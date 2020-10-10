b4b7a0f96b6240de739365c0083fdb3210507eb9
Ali-9005 <70147853+Ali-9005@users.noreply.github.com>
2020-10-10 19:57:57 +0300

    Done

--- scripts.js
+++ scripts.js
@@ -1,12 +1,42 @@
 // 1
 // moon.jpg قم بالوصول إلى الصورة وتغيرها إلى
 
+// let image = document.getElementById("image");
+// image.src = "./images/moon.jpg";
+
 // 2
 // "قم بالوصول إلى كلمة الأرض وتحويلها إلى "القمر
+
+// let text = document.getElementById("caption");
+// text.innerHTML = "القمر"
 
 // 3
 // بحيث إن ضغطت على البطاقة تتغير الصورة للصورة الأخرى ويتغير الاسم onClick قم بإنشاء دالة واستخدام خاصية
 // دون الحاجة إلى الخطوتين 2و1
+
+function planet(){
+    // let image = document.getElementById("image");
+    // let body = document.getElementById("body");
+    // image.src = "./images/moon.jpg";
+    // body.style.background = "gray";
+    
+
+    // let text = document.getElementById("caption");
+    //  text.innerHTML = "القمر"
+
+     if(document.getElementById("caption").innerHTML == "الشمس"){
+        let image = document.getElementById("image");
+        let text = document.getElementById("caption");
+        image.src = "./images/sun.jpg";
+        text.innerHTML = "القمر";
+        document.body.style.background = "gray";
+
+        
+     } else if(document.getElementById("caption").innerHTML == "القمر");
+        image.src = "./images/moon.jpg";
+        text.innerHTML = "الشمس";
+        document.body.style.background = "white";
+}
 
 // 4
 // Night - Light Mode قم بالتعديل على الدالة بحيث تضم إليها خاصية
